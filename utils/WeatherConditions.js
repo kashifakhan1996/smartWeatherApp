92518673c97103d09d6135aea6562290b70b9300
kashifakhan1996 <khankashifa.1996@gmail.com>
2021-07-08 22:16:19 +0530

    first commit

---
+++ utils/WeatherConditions.js
@@ -0,0 +1,51 @@
+export const weatherConditions = {
+  Rain: {
+    color: '#005BEA',
+    title: 'Raining',
+    subtitle: 'Get a cup of coffee',
+    icon: 'weather-rainy'
+  },
+  Clear: {
+    color: '#f7b733',
+    title: 'So Sunny',
+    subtitle: 'Sun Shine',
+    icon: 'weather-sunny'
+  },
+  Thunderstorm: {
+    color: '#616161',
+    title: 'A Storm is coming',
+    subtitle: 'Storming',
+    icon: 'weather-lightning'
+  },
+  Clouds: {
+    color: '#1F1C2C',
+    title: 'Clouds',
+    subtitle: 'Everywhere',
+    icon: 'weather-cloudy'
+  },
+
+  Snow: {
+    color: '#00d2ff',
+    title: 'Snow',
+    subtitle: 'Snowyy',
+    icon: 'weather-snowy'
+  },
+  Drizzle: {
+    color: '#076585',
+    title: 'Drizzle',
+    subtitle: 'Partially raining...',
+    icon: 'weather-hail'
+  },
+  Haze: {
+    color: '#66A6FF',
+    title: 'Haze',
+    subtitle: 'Partial Raining',
+    icon: 'weather-hail'
+  },
+  Mist: {
+    color: '#3CD3AD',
+    title: 'Mist',
+    subtitle: "Don't roam in forests!",
+    icon: 'weather-fog'
+  }
+};
